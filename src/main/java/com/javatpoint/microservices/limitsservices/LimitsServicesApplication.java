9670bec55533439ddd49c24013ee8fa249cf724d
sumayyayellow <sumayyayellow@gmail.com>
2022-08-13 14:13:53 -0500

    first commit

---
+++ src/main/java/com/javatpoint/microservices/limitsservices/LimitsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint.microservices.limitsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServicesApplication.class, args);
+	}//I will create now the endpoint to your microservice
+
+}
---
+++ src/main/java/com/javatpoint/microservices/limitsservices/controller/LimitsController.java
@@ -0,0 +1,12 @@
+package com.javatpoint.microservices.limitsservices.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//In java we use annotationsto create a controller, in Spring Boot we use the RestController annotation
+@RestController
+//This class is a Rest controller now, to mapping our url pat we use RequestMapping
+@RequestMapping
+public class LimitsController {
+
+}
